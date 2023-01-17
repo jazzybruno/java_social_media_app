a1acef6236e63af0bfcaee9e37ba087e93e376f5
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-01-17 08:25:21 +0200

    Added the post class

--- src/main/java/rca/ac/rw/orm/Posts.java
+++ src/main/java/rca/ac/rw/orm/Posts.java
@@ -1,4 +1,68 @@
 package rca.ac.rw.orm;
 
+import java.util.ArrayList;
+
 public class Posts {
+    public int id;
+    public String description;
+    public ArrayList<String> postPhotos = new ArrayList<String>();
+    public User user;
+    public int likes;
+    public int comments;
+    public Posts(int id, String description, ArrayList<String> postPhotos, User user, int likes, int comments) {
+        this.id = id;
+        this.description = description;
+        this.postPhotos = postPhotos;
+        this.user = user;
+        this.likes = likes;
+        this.comments = comments;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getPostPhotos() {
+        return postPhotos;
+    }
+
+    public void setPostPhotos(ArrayList<String> postPhotos) {
+        this.postPhotos = postPhotos;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public void setComments(int comments) {
+        this.comments = comments;
+    }
 }
