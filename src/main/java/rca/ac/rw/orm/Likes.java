a5aad40d57dc5c090519cba4a10ab99030f0d6b7
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-01-17 08:46:21 +0200

    Added the likes class in the project

--- src/main/java/rca/ac/rw/orm/Likes.java
+++ src/main/java/rca/ac/rw/orm/Likes.java
@@ -1,5 +1,49 @@
 package rca.ac.rw.orm;
 
-public class Likes {
+import java.util.Date;
 
+public class Likes {
+private int id;
+private Posts posts;
+private User user;
+private Date date;
+
+    public Likes(int id, Posts posts, User user, Date date) {
+        this.id = id;
+        this.posts = posts;
+        this.user = user;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Posts getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Posts posts) {
+        this.posts = posts;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
