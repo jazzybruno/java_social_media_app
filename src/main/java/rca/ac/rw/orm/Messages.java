b5a4d030dc187c1b57603e49cd2ad0c4a8cb3e40
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-01-17 09:07:55 +0200

    Added the messages classes in the project

--- src/main/java/rca/ac/rw/orm/Messages.java
+++ src/main/java/rca/ac/rw/orm/Messages.java
@@ -1,4 +1,59 @@
 package rca.ac.rw.orm;
 
+import java.util.Date;
+
 public class Messages {
+    private int id;
+    private String message;
+    private Date date;
+    private  User sender;
+    private User receiver;
+
+    public Messages(int id, String message, Date date, User sender, User receiver) {
+        this.id = id;
+        this.message = message;
+        this.date = date;
+        this.sender = sender;
+        this.receiver = receiver;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
 }
