062535d8875df70d056b7f01b821e7e8a7db144d
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-01-17 08:51:11 +0200

    Added  the notification class in the project

--- src/main/java/rca/ac/rw/orm/Notifications.java
+++ src/main/java/rca/ac/rw/orm/Notifications.java
@@ -1,4 +1,50 @@
 package rca.ac.rw.orm;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
+
 public class Notifications {
+    private int id;
+    private String content;
+    private boolean isRead;
+    private Date date;
+
+    public Notifications(int id, String content, boolean isRead, Date date) {
+        this.id = id;
+        this.content = content;
+        this.isRead = isRead;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
