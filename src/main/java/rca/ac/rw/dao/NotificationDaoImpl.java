600d8ece585a1c72197bd0f6a15eb63b7ce3f027
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-01-25 20:56:48 +0200

    Finished the method of updating , deleting and getting users from the database

--- src/main/java/rca/ac/rw/dao/NotificationDaoImpl.java
+++ src/main/java/rca/ac/rw/dao/NotificationDaoImpl.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import rca.ac.rw.orm.Notification;
 
+import java.util.List;
+
 public class NotificationDaoImpl implements NotificationDao{
     Session session;
     Transaction transaction;
@@ -25,7 +27,43 @@ public class NotificationDaoImpl implements NotificationDao{
            e.printStackTrace();
        }
 
+    }
 
+    public List<Notification> getNotifications(){
+        return session.createCriteria(Notification.class).list();
+    }
 
+    public void updateNotification(Notification notification){
+        transaction = null;
+        try {
+            transaction =  session.beginTransaction();
+            session.saveOrUpdate(notification);
+            transaction.commit();
+            session.close();
+        }catch (Exception e){
+            if (transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteNotification(int id){
+        transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            Notification notification = (Notification) session.get(Notification.class , id);
+
+            if (notification != null){
+               session.delete(notification);
+            }
+            transaction.commit();
+            session.close();
+        }catch (Exception e){
+            if(transaction != null){
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 }
